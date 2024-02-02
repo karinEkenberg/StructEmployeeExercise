45186b7f777951ceee32693d33b96fc72b760f51
karinEkenberg <zirkon6@hotmail.com>
2024-02-02 13:26:34 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructEmployeeExercise
+{
+    struct Employee
+    {
+        public string name;
+        public dateOfBirth Date;
+    }
+
+    struct dateOfBirth
+    {
+        public int Day;
+        public int Month;
+        public int Year;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StructEmployeeExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dd = 0, mm = 0, yy = 0;
+            int total = 2;
+            Console.WriteLine("Creating a nested structure to store data in an array:");
+            Console.WriteLine("------------------------------------------------------");
+            Employee[] emp = new Employee[total];
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine("Enter name of the employee:");
+                string eName = Console.ReadLine();
+                emp[i].name = eName;
+
+                Console.WriteLine("Enter day of birth:");
+                dd = int.Parse(Console.ReadLine());
+                emp[i].Date.Day = dd;
+
+                Console.WriteLine("Enter month of birth:");
+                mm = int.Parse(Console.ReadLine());
+                emp[i].Date.Month = mm;
+
+                Console.WriteLine("Enter year of birth:");
+                yy = int.Parse(Console.ReadLine());
+                emp[i].Date.Year = yy;
+            }
+        }
+    }
+}
